c2a7c4155e5800c7c336bb81dcb25b366a1510fd
Sholaropo <ropoolusola@gmail.com>
2025-03-08 22:30:04 +0100

    feat: added user and admin routes and controllers

---
+++ src/app.ts
@@ -0,0 +1,47 @@
+import express, { Express } from "express";
+import morgan from "morgan";
+// import setupSwagger from "../config/swagger";
+// import itemRoutes from "./api/v1/routes/itemRoutes";
+import userRoutes from "./api/v1/routes/userRoutes";
+import adminRoutes from "./api/v1/routes/adminRoutes";
+import errorHandler from "./api/v1/middleware/errorHandler";
+
+const app: Express = express();
+
+// setupSwagger(app);
+
+app.use(morgan("combined"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello, world!");
+});
+
+// define GET route for health check
+/**
+ * @openapi
+ * /api/v1/health:
+ *  get:
+ *   summary: Get health status of the application
+ *   tags: [Health]
+ *   responses:
+ *    200:
+ *     description: The application's status, uptime, the current timestamp, and version
+ */
+app.get("/api/v1/health", (req, res) => {
+    res.json({
+        status: "OK",
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString(),
+        version: "1.0.0",
+    });
+    // send JSON response with status, server uptime, current time, API version
+});
+
+// app.use("/api/v1/items", itemRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/admin", adminRoutes);
+
+app.use(errorHandler);
+
+export default app;
